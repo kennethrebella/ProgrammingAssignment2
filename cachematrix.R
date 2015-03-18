d2eaddcae670f06f322479fdbef9c3c90dee2f7c
Kenneth Rebella <kennethrebella@Kenneths-MacBook-Air.local>
2015-03-18 15:52:57 -0700

    submitting assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This function essentially creates a list of functions. 
+## When we passing the functions within a function creates an environment 
+## where set, get, setmatrix,and getmatrix are defined 
+## it also caches variable m to the parent environment
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        get<-function() x
+        setmatrix<-function(solve) m<<- solve
+        getmatrix<-function() m
+        list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve pulls up the values established when set is called
+## it then takes that value and runs the solve function on it
+## which returns the inverse of a square matrix. 
+## It then sets the matrix and finally gives the inverted matrix 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m<-x$getmatrix()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        matrix<-x$get()
+        m<-solve(matrix, ...)
+        x$setmatrix(m)
+        m
 }
